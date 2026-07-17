3c6817c7e00f5ef3d3627e280a08fa8639cb610a
Kien Quyet <kienquyet1201@gmail.com>
2026-07-17 22:28:35 +0700

    clean server

--- public/check_html.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('C:/Users/PC/Downloads/VieGeoKhoiNguyen-main/VieGeoKhoiNguyen/public/map.html', 'utf8');
-
-// Just write the top 150 lines to a scratch file to read
-const topHtml = html.split('\n').slice(0, 150).join('\n');
-fs.writeFileSync('C:/Users/PC/Downloads/VieGeoKhoiNguyen-main/VieGeoKhoiNguyen/public/scratch.txt', topHtml, 'utf8');
--- public/dump_html.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('C:/Users/PC/Downloads/VieGeoKhoiNguyen-main/VieGeoKhoiNguyen/public/map.html', 'utf8');
-
-// Extract Sidebar and Header
-const sidebarMatch = html.match(/<aside[^>]*>[\s\S]*?<\/aside>/);
-const headerMatch = html.match(/<div class="stats-header[^>]*>[\s\S]*?<\/div>\s*<\/div>/);
-
-let dump = "=== SIDEBAR ===\n" + (sidebarMatch ? sidebarMatch[0] : "Not found") + "\n\n=== HEADER ===\n" + (headerMatch ? headerMatch[0] : "Not found");
-fs.writeFileSync('C:/Users/PC/Downloads/VieGeoKhoiNguyen-main/VieGeoKhoiNguyen/public/dump.txt', dump, 'utf8');
